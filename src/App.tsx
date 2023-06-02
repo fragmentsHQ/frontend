37750e3f0f6c54c06a4ee5cd39eaeb972e65e218
lordmike007 <manujasan23@gmail.com>
2023-06-03 04:53:42 +0530

    task page ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useAccount } from "wagmi";
 import { useAutoConnect } from "./useAutoConnect";
 import { Route, Routes } from "react-router";
 import Main from "./screens/Main";
-import JobDetails from "./components/JobDetails";
 import Balance from "./screens/Balance";
+import Task from "./screens/Task";
 
 const App = () => {
   const { address } = useAccount();
@@ -13,7 +13,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="balance/" element={<Balance />} />
-      <Route path="job/:jobId" element={<JobDetails />} />
+      <Route path="job/:jobId" element={<Task />} />
     </Routes>
   );
 };
