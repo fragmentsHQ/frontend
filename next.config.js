7a8d8bdae02bbbf6981100ef1de58d7eec939a16
Priyanshu Kumar Panda <priyanshupanda.ctp@gmail.com>
2023-06-22 09:38:24 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -15,7 +15,12 @@ const nextConfig = {
             'goerli.etherscan.io',
             'docs.matic.network'
         ],
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
