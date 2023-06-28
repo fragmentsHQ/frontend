f0e448277ccf0f2164c095e077c6d4425e42e2b7
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-06-28 12:47:21 +0530

    fix: deploy on testnet

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,5 +1,7 @@
-export const isProd = process.env.NODE_ENV === 'production';
-export const isLocal = process.env.NODE_ENV === 'development';
+// export const isProd = process.env.NODE_ENV === 'development';
+// export const isLocal = process.env.NODE_ENV === 'production';
+export const isProd = false;
+export const isLocal = true;
 
 export const showLogger = isLocal
   ? true
